80adc1dff32fe33472a988c49ffe2073533a9074
samih freire <samihcisne@gmail.com>
2025-09-03 11:56:58 -0300

    feat: instalando HTML-Webpack-Plugin e configurando Webpack para incluir o html da aplicação

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin") //Importando o plugin
 
 module.exports = {
     target: "web",
@@ -18,4 +19,10 @@ module.exports = {
         open: true, //Sempre que executar o devServer abrir aplicação
         liveReload: true, //Sempre que modificar ele carregar aplicação
     },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "index.html"),
+        }),
+    ],
 }
