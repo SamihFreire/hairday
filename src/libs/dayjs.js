bd9b2c23a4f783e8f36b9ddbc2c8e6b620abd81c
samih freire <samihcisne@gmail.com>
2025-09-03 17:38:02 -0300

    feat: instalado e configurado o pacote day.js

---
+++ src/libs/dayjs.js
@@ -0,0 +1,4 @@
+import dayjs from "dayjs"
+import "dayjs/locale/pt-br" //A importação do idioma
+
+dayjs.locale("pt-br") //Carrega o idioma que vai ser utilizado na aplicação
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 "use strict"
 
+//Configuração do dayjs
+import "./libs/dayjs"
+
 //CSS
 import "./styles/global.css"
 import "./styles/form.css"
