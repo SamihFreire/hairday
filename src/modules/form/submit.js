16ed90f423f7c51511afc194910a3fa7eb1429e7
samih freire <samihcisne@gmail.com>
2025-09-04 16:33:32 -0300

    feat: enviando um horário selecionado para a API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,16 +1,54 @@
 import dayjs from "dayjs"
 
 const form = document.querySelector("form");
-const selectDate = document.getElementById("date");
+const clientName = document.getElementById("client");
+const selectedDate = document.getElementById("date");
 
 //Date atual para formatar o input
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
 
 //Carrega a data atual e define a data mínima como sendo a data atual
-selectDate.value = inputToday
-selectDate.min = inputToday;
+selectedDate.value = inputToday
+selectedDate.min = inputToday;
 
 form.onsubmit = (event) => {
     // Previne o comportamento padrão de carregar a página
     event.preventDefault();
+
+    try {
+        //Recuperando o nome do cliente
+        const name = clientName.value.trim();
+
+        //Se a variável name tiver valor vai retornar true se for vazia retornar falso
+        if(!name) { //Se name vazia entra no if
+            return alert("Informe o nome do cliente!");
+        }
+
+        //Recupera o horário selecionado
+        const hourSelected = document.querySelector(".hour-selected");
+        
+        //Recuperando o horário selecionado
+        if(!hourSelected) {
+            return alert("Selecione a hora!");
+        }
+
+        //Recuperar somente a hora
+        const [hour] = hourSelected.innerText.split(":");
+        console.log(hour);
+
+        //Insere a hora na data
+        const when = dayjs(selectedDate.value).add(hour, "hour");
+        
+        
+        //Gerar um IF
+        const id = new Date().getTime();
+
+        console.log({
+            id, name, when
+        })
+
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento.")
+        console.log(error)
+    }
 }
