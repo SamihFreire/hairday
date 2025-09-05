198f0d5093170c471e96a9973b7203b77159aa74
samih freire <samihcisne@gmail.com>
2025-09-05 16:48:17 -0300

    feat: criado endpoint de busca dos agendamentos por dia e renderização dos agendamentos

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,14 +1,22 @@
 //Esse load carrega o agendamento do dia
 
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
+import { schadulesShow } from "../schedules/show.js"
 import { hoursLoad } from "../form/hours-load.js"
 
 //Seleciona o input de data
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
     //Obtém a data do input
     const date = selectedDate.value;
 
+    //Busca na API os agendamentos
+    const dailySchedules = await scheduleFetchByDay({ date });
+    
+    //Exibe os agendamentos
+    schadulesShow({ dailySchedules });
+
     //Renderiza as horas disponíveis
-    hoursLoad( {date} );
+    hoursLoad({ date });
 }
